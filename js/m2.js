b371fd876db8ca1dab8a47dfcadfe06ff8b59839
ivaneric79 <ivaneric79@gmail.com>
2013-09-06 02:14:14 -0500

    test

--- js/m2.js
+++ js/m2.js
@@ -5,4 +5,17 @@
     //
     function onDeviceReady() {
       alert(device.name);
+
+bluetoothSerial.connect('00:15:0E:E1:D3:53', connectSuccess, connectFailure);
+function connectSuccess() {
+	alert('OK');
+
+	// body...
+}
+
+function connectFailure() {
+	alert('no');
+}
+
+
     }
